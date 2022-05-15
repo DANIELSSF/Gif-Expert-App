cf6c8a135b7b6e9b1c8812530b0a71661fe16fad
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-15 17:44:18 -0500

    component principal with a object of anime

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    // const categories = ['High School DXD', 'Princess re:conected drive', 'Fate', 'Date a Live'];
+    //const [categories, setcategories] = useState(['High School DXD', 'Princess re:conected drive', 'Fate', 'Date a Live']);
+    const [categories, setcategories] = useState(['Date a Live']);
+
+    //const handleAdd = () => setcategories(cate => [...cate, 'Dragon ball']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setcategories={setcategories} />
+            <hr />
+            <ol>
+                {
+                    categories.map(category =>
+                        <GifGrid
+                            key={category}
+                            category={category} />
+                    )
+                }
+            </ol>
+
+        </>
+    );
+
+}
