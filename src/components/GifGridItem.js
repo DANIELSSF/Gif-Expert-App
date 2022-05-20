fb6d952e6647f76f9fda9710d893f2cbb222a4e2
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-20 13:48:24 -0500

    docs directory was removed

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({ id, tittle, url }) => {
 
     return (
-        <div className='card animate__animated animate__bounceIn'>
+        <div className='card animate__animated animate__bounceIn    '>
             
             <img src={url} alt={tittle}></img>
             <p>{tittle}</p>
