b896ac646c49c1d050784a01bd5a392deb87ebbd
DANIELSSF <daniel.santiago730@gmail.com>
2022-08-10 15:16:34 -0500

    prop-types was added

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 
 export const GifItem = ({ title, url, id }) => {
 
@@ -8,4 +8,9 @@ export const GifItem = ({ title, url, id }) => {
         <p>{ title }</p>
     </div>
   )
+}
+
+GifItem.propTypes={
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 }
