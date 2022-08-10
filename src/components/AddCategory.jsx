c65bbb11eb55e81a96cbf8c3498eafdc5fcf9d1f
DANIELSSF <daniel.santiago730@gmail.com>
2022-08-10 15:16:13 -0500

    prop-types was added

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,31 +1,38 @@
 import { useState } from 'react';
-
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
 
-    const [ inputValue, setInputValue ] = useState('');
+    const [inputValue, setInputValue] = useState('');
 
     const onInputChange = ({ target }) => {
-        setInputValue( target.value );
+        setInputValue(target.value);
     }
 
-    const onSubmit = ( event ) => {
+    const onSubmit = (event) => {
         event.preventDefault();
-        if( inputValue.trim().length <= 1) return;
+        if (inputValue.trim().length <= 1) return;
 
         // setCategories( categories => [ inputValue, ...categories ]);
         setInputValue('');
-        onNewCategory( inputValue.trim() );
+        onNewCategory(inputValue.trim());
     }
 
     return (
-        <form onSubmit={ onSubmit }>
-            <input 
+        <form
+            onSubmit={onSubmit}
+            aria-label='form'
+        >
+            <input
                 type="text"
                 placeholder="Buscar gifs"
-                value={ inputValue }
-                onChange={ onInputChange }
+                value={inputValue}
+                onChange={onInputChange}
+                aria-label='input-search'
             />
         </form>
     )
+}
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
